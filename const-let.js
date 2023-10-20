203f152dd6e27a6d5976968546a6fa02faa2b281
tf292201 <tf292201@gmail.com>
2023-10-20 15:41:40 -0400

    initial commit

---
+++ const-let.js
@@ -0,0 +1,12 @@
+1.
+const PI = 3.14
+
+
+2. var can be re-assigned and re-declared. let can be re-assigned but not re-declared. let is block scoped. 
+ var is hoisted.
+
+3. var can be re-assigned and re-declared. const is block scoped, immutable, and is not hoisted like var.
+
+4. let and const are both block scoped, but const cannot be re-assigned or re-declared like let.
+
+5. When Javascript is compiled it is hoisted to the top of the scope. It is intialized to undefined at the top of the code before it is assigned a value.
